45a9b13ebf424f95618b5a71a98076375dcab20c
carlos garcia <ambro_93garcas@hotmail.com>
2022-10-23 15:09:57 -0500

    migraciones de los modelos y registro al aadmin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
+from email.policy import default
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date publishbed")
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
