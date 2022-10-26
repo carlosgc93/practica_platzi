736cf84ea54c5460ff165b30dd9f7f8e15f0dbbd
carlos garcia <ambro_93garcas@hotmail.com>
2022-10-26 17:24:51 -0500

    agregando opciones al admin site

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 # Register your models here.
-admin.site.register([Question,Choice])
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields= ["pub_date", "question_text"]
+    list_display= ("question_text", "pub_date")
+    list_filter= ["pub_date"]
+    search_fields= ["question_text"]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_filter= ["choice_text"]
+    search_fields= ["choice_text"]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
