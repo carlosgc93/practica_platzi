2abb62046625b6b8ec4c5d17aee1949cab4dde83
Carlos Garcia <carlosgc93@users.noreply.github.com>
2022-10-26 20:58:08 -0500

    Update settings.py

--- platzipremios/settings.py
+++ platzipremios/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1)ccqe9tp+%_kh=4x41&ud4fu==!j9i%+m(yvx_7!5ba8c^vr+'
+SECRET_KEY = 'xxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
